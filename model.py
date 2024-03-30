4bef2f7d4eb8d57cb98ae67f394db2f2840444dc
KDC <1664969415@qq.com>
2024-03-30 14:16:52 +0800

    修改：     model.py

--- model.py
+++ model.py
@@ -2,4 +2,28 @@ import os
 import torch
 import torch.nn as nn
 
-n =10
+class Attention(nn.module):
+    def __init__(self,
+                 dim,
+                 num_heads = 8,
+                 qkv_bias = False,
+                 attn_drop = 0,
+                 proj_drop = 0):
+        super().__init()
+        # self.pos_bias = nn.Parameter(torch.randn(num_head, ))
+        self.num_head = num_heads
+        # qkv向量长度
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -o.5
+        
+
+
+
+class nnvit(nn.module):
+    def __init__(self,
+                 Dim = 128,
+                 num_head = 8,
+                 out_dim = 32):
+        
+    
+    
